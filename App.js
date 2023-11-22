765f8554a790f45f217931a0a124f13f31cff312
rahul singh <116128750+rahulroyalsingh@users.noreply.github.com>
2023-11-22 16:28:21 +0530

    Display list using Bootstrap

---
+++ App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React from 'react'
+import{Table}from 'react-bootstrap'
+function App() {
+const users=[
+  {name:"raj",email:"raj@test.com", contact:123},
+  {name:"sid",email:"sid@test.com", contact:234},
+  {name:"marry",email:"marry@test.com", contact:456},
+  {name:"alex",email:"alex@test.com", contact:567},
+]
+  return(
+    <div className="App">
+    <h1> List with Bootstrap Table</h1>
+    <Table>
+      <tr>
+      
+        <td>Name</td>
+        <td>Email</td>
+        <td>Contact</td>
+      </tr>
+      
+       {
+      users.map((item)=>
+      <tr>
+        <td>{item.name}</td>
+        <td>{item.email}</td>
+        <td>{item.contact}</td>
+      </tr>
+      )
+    }
+      </Table>       
+     </div>
+    );
+  } 
+export default App; 
